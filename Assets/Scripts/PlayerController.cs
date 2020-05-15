60a29948392c3768610a39ffa2fb9c8900149858
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-05-14 23:13:26 -0400

    INIT: movement speed too fast

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public float speed = 50f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+         	if (Input.GetKey(KeyCode.A))
+             		rb.AddForce(Vector3.left * speed);
+         	if (Input.GetKey(KeyCode.D))
+             		rb.AddForce(Vector3.right * speed);
+         	if (Input.GetKey(KeyCode.W))
+         		rb.AddForce(Vector3.forward * speed);
+         	if (Input.GetKey(KeyCode.S))
+             		rb.AddForce(Vector3.forward * -speed);
+		
+	}
+}
